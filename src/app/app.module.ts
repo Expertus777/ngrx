1a3b17338b342e2e8744d159a0224e7a3dc84f8b
Ruslan Behun <r.behun@mobilunity.com>
2019-04-04 12:51:11 +0300

    finish my-counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
+import { MyCounterComponent } from './components/my-counter/my-counter.component';
+
+import { counterReducer } from './counter.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/my-counter/my-counter.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/index';
+import { Store, select} from '@ngrx/store';
+import { Increment, Decrement, Reset } from '../../counter.action';
+
+@Component({
+  selector: 'ngrx-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.scss']
+})
+export class MyCounterComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{count: number}>) {
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    this.store.dispatch(new Increment());
+  }
+
+  decrement() {
+    this.store.dispatch(new Decrement());
+  }
+
+  reset() {
+    this.store.dispatch(new Reset());
+  }
+}
